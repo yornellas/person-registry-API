f7a2964e3b3e33bfdca5b8c2401978f42fc576b4
Yolanda Ornellas <ymf.ornellas@icloud.com>
2021-08-15 11:44:02 -0300

    create project

---
+++ src/main/java/com/github/yornellas/personregistryapi/PersonRegistryApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.yornellas.personregistryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonRegistryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonRegistryApiApplication.class, args);
+	}
+
+}
